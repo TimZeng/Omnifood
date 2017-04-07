6755dc7ee709172309018a7d17c33566257c4cce
Tim Zeng <zengtiantian1122@gmail.com>
2017-04-06 21:47:11 -0700

    add scroll animation when click on buttons

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,8 +1,42 @@
 $(document).ready(function() {
 
-  $('h1').click(function() {
-    $(this).css('background-color', '#ff0000');
-  })
+  // for the sticky navigation
+  $('.js--section--feature').waypoint(function(direction) {
+    if ( direction === 'down' ) {
+      $('nav').addClass('sticky');
+    } else {
+      $('nav').removeClass('sticky');
+    }
+  },{
+    offset: '60px;'
+  });
+
+  // Scroll on button clicks
+  $('.js--scroll-to-plans').click(function() {
+    console.log('I am hungery clicked');
+    $('html, body').animate({scrollTop: $('.js--section--plans').offset().top}, 1000);
+  });
+
+  $('.js--scroll-to-start').click(function() {
+    console.log('I am hungery clicked');
+    $('html, body').animate({scrollTop: $('.js--section--feature').offset().top}, 1000);
+  });
+
+  // Navigation sroll
+  $(function() {
+    $('a[href*="#"]:not([href="#"])').click(function() {
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+          $('html, body').animate({
+            scrollTop: target.offset().top
+          }, 1000);
+          return false;
+        }
+      }
+    });
+  });
 
 
 })
